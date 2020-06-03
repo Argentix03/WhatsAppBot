bfa1ef6db0dd65cfb0ad7839feba3bd7b9ceaa14
Cheka <39255806+Argentix03@users.noreply.github.com>
2020-06-03 19:39:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ if os.name == 'nt':
         chromeversion = chromeversion[2].replace('\n', '')
         print(f"Chrome Version: {chromeversion}")
         filepath = "https://chromedriver.storage.googleapis.com/" + chromeversion + "/chromedriver_win32.zip"
-        os.system(f"curl {filepath} -o chromedriver3.exe")
-        print(f"curl {filepath} -o chromedriver3.exe")
+        os.system(f"curl {filepath} -o chromedriver2.exe")
+        print(f"curl {filepath} -o chromedriver2.exe")
 else:
     #linux/mac (posix) here
     pass
