78ed6b29e27113383bd0b3a519f2375d65e3ab59
Cheka <39255806+Argentix03@users.noreply.github.com>
2020-06-04 17:58:37 +0300

    Update downloadtest.py

--- downloadtest.py
+++ downloadtest.py
@@ -3,7 +3,7 @@ s = requests.Session()
 s.get('https://chromedriver.storage.googleapis.com/index.html?path=83.0.4103.39/')
 s.get('https://chromedriver.storage.googleapis.com/index.html?path=83.0.4103.39/')
 r = s.get('https://chromedriver.storage.googleapis.com/83.0.4103.39/chromedriver_win32.zip')
-with open('chromedriver2.exe','wb') as file:
+with open('chromedriver_win32.zip','wb') as file:
     file.write(r.content)
     file.close()
 print('saved file')
