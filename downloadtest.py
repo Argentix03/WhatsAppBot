3f651e0076ec5d30ae39c74514dee7fbd5456525
Cheka <39255806+Argentix03@users.noreply.github.com>
2020-06-04 19:57:22 +0300

    Delete downloadtest.py

--- downloadtest.py
+++
@@ -1,9 +0,0 @@
-import requests
-s = requests.Session()
-s.get('https://chromedriver.storage.googleapis.com/index.html?path=83.0.4103.39/')
-s.get('https://chromedriver.storage.googleapis.com/index.html?path=83.0.4103.39/')
-r = s.get('https://chromedriver.storage.googleapis.com/83.0.4103.39/chromedriver_win32.zip')
-with open('chromedriver_win32.zip','wb') as file:
-    file.write(r.content)
-    file.close()
-print('saved file')
