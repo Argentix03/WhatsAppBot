7d914249ca064a419a879a181b1327aa5efa1133
Cheka <39255806+Argentix03@users.noreply.github.com>
2020-06-04 20:13:17 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import requests
 import os
-print(f"System: {os.name}")
+
+class downloadError(Exception):
+    pass
+class unzipError(Exception):
+    pass
 
 # Download the file using requests
 # google chrome change versions micro versions (which changes the download link too)
 # faster than i can write code so it has to find latest micro version for main version
 # if that makes any sense...
+# Returns the file name that was saved
 def downloadFile(version, system):
     baseURL = r'https://chromedriver.storage.googleapis.com/'
     winUrl =  r'chromedriver_win32.zip'
@@ -31,21 +36,35 @@ def downloadFile(version, system):
         file.write(req.content)
         file.close()
     print(f'Saved file: {fileURL}')
-    return fileURL
+    return fileURL  # The file name
+# Unzip the file, Return True on Success
+def unzipFile(filename):
+    return False
+
 
 # Get system, chrome version and pass to downloadFile()
-if os.name == 'nt':
-    chromeversion = os.popen('chromeversion.bat').read()
-    if "pv    REG_SZ    " in chromeversion:
-        chromeversion = chromeversion.split('REG_SZ    ')
-        chromeversion = chromeversion[2].replace('\n', '')
-        print(f"Chrome Version: {chromeversion}")
-        filename = downloadFile(chromeversion, 'windows')
-        if filename != None:
-            unzipFile(filename)
-        else:
-            print("Something went horribly wrong, Bye!")
-            exit()
-else:
-    #linux/mac (posix) here
-    pass
+try:
+    print(f"System: {os.name}")
+    if os.name == 'nt':
+        chromeversion = os.popen('chromeversion.bat').read()
+        if "pv    REG_SZ    " in chromeversion:
+            chromeversion = chromeversion.split('REG_SZ    ')
+            chromeversion = chromeversion[2].replace('\n', '')
+            print(f"Chrome Version: {chromeversion}")
+            filename = downloadFile(chromeversion, 'windows')
+            if filename != None:
+                if not(unzipFile(filename)):
+                    raise unzipError
+            else:
+                raise downloadError  # just random different error instead of making a new error class   
+    
+    else:
+        #linux/mac (posix) here
+        pass
+
+except downloadError:
+    print("Something went wrong!")
+    print("Instead of using setup.py you can download the right chrome webdriver for your chrome version from https://chromedriver.chromium.org/downloads and put in the same directory.")
+except unzipError:
+    print("Something went wrong!")
+    print("Please unzip the chrome driver manually")
